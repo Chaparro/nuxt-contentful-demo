4fcd5c1321b4bfe68132e4803582724e3ad20aaa
Chaparro <ricardoxac@gmail.com>
2020-07-28 04:10:38 -0500

    initial commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <h1 class="site-title">
             <nuxt-link :to="{ path: '/' }" class="site-title__link">
-                Nuxt Demo
+                Dreamavarium
             </nuxt-link>
         </h1>
         <nuxt/>
