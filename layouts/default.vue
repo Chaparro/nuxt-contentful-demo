4cc8025317e637bfb6b8ef5cc2c0f3d9afd8fbb4
Chaparro <ricardoxac@gmail.com>
2020-07-30 16:02:58 -0500

    image test

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <h1 class="site-title">
             <nuxt-link :to="{ path: '/' }" class="site-title__link">
-                <img src="../assets/drim.png" alt="Drim">
+                <img src="~/assets/drim.png" alt="Drim">
             </nuxt-link>
         </h1>
         <nuxt/>
