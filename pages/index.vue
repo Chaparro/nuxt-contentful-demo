857a9d465cea1dadbf22f95b74bd03b7ae1c6d68
Chaparro <ricardoxac@gmail.com>
2020-08-07 19:57:43 -0500

    footer test

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,19 @@
         <h2 class="title">Dreamavarium</h2>
         <ul>
             <li v-if="posts" v-for="(post, index) in posts" :key="index">
-                <nuxt-link :to="post.fields.slug">{{ post.fields.title }}</nuxt-link>
+                <nuxt-link :to="post.fields.slug">{{
+                    post.fields.title
+                }}</nuxt-link>
             </li>
         </ul>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>Dreamavarium</strong> by
+                    <a href="https://twitter.com/Dreamavariums">Ricardo Ch.</a>.
+                </p>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -13,11 +23,11 @@
 export default {
     computed: {
         posts() {
-            return this.$store.state.posts.posts
+            return this.$store.state.posts.posts;
         }
     },
     async fetch({ store, params }) {
-        await store.dispatch('posts/getPosts', params.slug)
+        await store.dispatch("posts/getPosts", params.slug);
     }
-}
+};
 </script>
